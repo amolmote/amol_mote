2ec8cf3a5b44a81dd64bb16817b233c7df99cae0
Amol Mote <amolmote201@gmail.com>
2024-02-06 23:56:53 +0530

    removed asset files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ - deploy https://medium.com/@badreddine.boudaoud21/create-a-react-app-with-vite-and-deploy-it-on-github-48b82e19f821
 export default defineConfig({
   base: "/personal-portfolio/",
   plugins: [react()],
