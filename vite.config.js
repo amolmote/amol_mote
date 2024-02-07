39ff0b743f25b98339307cc3b2a1f11acaba4e03
Amol Mote <amolmote201@gmail.com>
2024-02-07 09:24:59 +0530

    change base url project name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/ - deploy https://medium.com/@badreddine.boudaoud21/create-a-react-app-with-vite-and-deploy-it-on-github-48b82e19f821
 export default defineConfig({
-  base: "https://amolmote.github.io/amol_mote/",
+  base: "https://amolmote.github.io/personal-portfolio/",
   plugins: [react()],
 });
